476b1835cc583fc49acb0f5f12de2ca7d0a99c22
endsub <phung.linh2010@gmail.com>
2017-01-29 02:39:33 +0100

    added segment and adding change handler

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,39 +1,58 @@
 import React from 'react'
-import { Checkbox, Grid, Menu } from 'semantic-ui-react'
-import json from 'json-loader!file.json';
+import { Checkbox, Grid, Menu, Segment, Form } from 'semantic-ui-react'
+// import json from 'json-loader!file.json';
 
-// import Data from './category_data_eventbrite'
-import Data from './cat_data.json';
-console.log(Data)
-const Categories = () => (
-  <Grid className ='filter-menu' >
-    <Grid.Row>
-        <Grid.Column width={3}>
+import Categories_Data from './category_data_eventbrite'
 
-        <Checkbox label='Music' />
-        <Checkbox label='Business & Professional' />
-        <Checkbox label='Food & Drink' />
-        <Checkbox label='Community & Culture' />
-        <Checkbox label='Performing & Visual Arts' />
-        <Checkbox label='Film, Media & Entertainment' />
-        <Checkbox label='Sports & Fitness' />
-        <Checkbox label='Health & Wellness' />
-        <Checkbox label='Science & Technology' />
-        <Checkbox label='Charity & Causes' />
-        <Checkbox label='Religion & Spirituality' />
-        <Checkbox label='Family & Education' />
-        <Checkbox label='Religion & Spirituality' />
-        <Checkbox label='Seasonal & Holiday' />
-        <Checkbox label='Government & Politics' />
-        <Checkbox label='Fashion & Beauty' />
-        <Checkbox label='Home & Lifestyle' />
-        <Checkbox label='Hobbies & Special Interest' />
-        <Checkbox label='Other' />
 
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+var categoriesData = Categories_Data();
 
-)
+
+class Categories extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {value:''};
+  }
+
+  handleChange = (e, {value}) =>{
+    this.setState ({value: value})
+    console.log(this.state.checked)
+  }
+  render () {
+    return (
+      <Grid className ='filter-menu'>
+        <Grid.Row>
+            <Grid.Column width={4}>
+            <Segment>
+              <Form.Field>
+                <Checkbox label='Music'  />
+                <Checkbox label='Business & Professional' value={this.state.checked} onChange={this.handleChange} />
+                <Checkbox label='Food & Drink' />
+                <Checkbox label='Community & Culture' />
+                <Checkbox label='Performing & Visual Arts' />
+                <Checkbox label='Film, Media & Entertainment' />
+                <Checkbox label='Sports & Fitness' />
+                <Checkbox label='Health & Wellness' />
+                <Checkbox label='Science & Technology' />
+                <Checkbox label='Charity & Causes' />
+                <Checkbox label='Religion & Spirituality' />
+                <Checkbox label='Family & Education' />
+                <Checkbox label='Religion & Spirituality' />
+                <Checkbox label='Seasonal & Holiday' />
+                <Checkbox label='Government & Politics' />
+                <Checkbox label='Fashion & Beauty' />
+                <Checkbox label='Home & Lifestyle' />
+                <Checkbox label='Hobbies & Special Interest' />
+                <Checkbox label='Other' />
+              </Form.Field>
+            </Segment>
+
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+}
+
 
 export default Categories
